8f39929bccfde1ca209cf7dcde148b6cac0b8761
olgamie <olga.mierzwa@gmail.com>
2017-01-02 11:29:50 +0100

    add function

--- app/app.R
+++ app/app.R
@@ -13,22 +13,28 @@ ui <- shinyUI(fluidPage(
 server <- shinyServer(function(input, output) {
   
   output$map <- leaflet::renderLeaflet({
-    leaflet::leaflet(ships) %>% leaflet::addTiles() %>% leaflet::addMarkers()
+    leaflet::leaflet(ships) %>%
+      leaflet::addTiles() %>%
+      leaflet::addMarkers()
   })
+  
+  in_bounding_box <- function(data, lat, long, bounds) {
+   data %>%
+      dplyr::filter(lat > bounds$south & lat < bounds$north & long < bounds$east & long > bounds$west)
+  }
   
   data_map <- reactive({
     if (is.null(input$map_bounds)){
       ships
     } else {
       bounds <- input$map_bounds
-      ships %>%
-        dplyr::filter(lat > bounds$south & lat < bounds$north & long < bounds$east & long > bounds$west)
+        in_bounding_box(ships, lat, long, bounds)
     }
   })
   
   output$tbl <- DT::renderDataTable({
     DT::datatable(data_map(), extensions = "Scroller", style = "bootstrap", class = "compact", width = "100%",
-                  options = list(deferRender = TRUE, scrollY = 300, scroller = TRUE))
+                  options = list(deferRender = TRUE, scrollY = 300, scroller = TRUE,  dom = 'tp'))
   })
   
 })
