c5353c3ae6d1968aa8bdf04653d128f49e732e27
Filip Stachura <filip@appsilon.pl>
2017-01-03 16:31:53 +0100

    Non-responsive UI.

--- app/app.R
+++ app/app.R
@@ -3,26 +3,24 @@ library(magrittr)
 
 ships <- read.csv("ships.csv")
 
-ui <- shinyUI(fluidPage(
-  fluidRow(
-    column(6, leaflet::leafletOutput("map")),
-    column(6, DT::dataTableOutput("tbl"))
-  )
+ui <- shinyUI(fixedPage(style = "padding: 0",
+    tags$div(style = "width: 50%; float: left", leaflet::leafletOutput("map")),
+    tags$div(style = "width: 50%; float: right", DT::dataTableOutput("tbl"))
 ))
 
 server <- shinyServer(function(input, output) {
-  
+
   output$map <- leaflet::renderLeaflet({
     leaflet::leaflet(ships) %>%
       leaflet::addTiles() %>%
       leaflet::addMarkers()
   })
-  
+
   in_bounding_box <- function(data, lat, long, bounds) {
    data %>%
       dplyr::filter(lat > bounds$south & lat < bounds$north & long < bounds$east & long > bounds$west)
   }
-  
+
   data_map <- reactive({
     if (is.null(input$map_bounds)){
       ships
@@ -31,13 +29,13 @@ server <- shinyServer(function(input, output) {
         in_bounding_box(ships, lat, long, bounds)
     }
   })
-  
+
   output$tbl <- DT::renderDataTable({
     DT::datatable(data_map(), extensions = "Scroller", style = "bootstrap", class = "compact", width = "100%",
                   options = list(deferRender = TRUE, scrollY = 300, scroller = TRUE,  dom = 'tp'))
   })
-  
+
 })
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
